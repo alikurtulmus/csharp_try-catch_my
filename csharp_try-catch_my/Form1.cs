958d1848248adc3d1c30276613d898101797866c
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-19 21:41:32 +0300

    Add project files.

---
+++ csharp_try-catch_my/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_try_catch_my
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sayi1 = Convert.ToInt32(textBox1.Text);
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                int toplam = sayi1 + sayi2;
+                MessageBox.Show("Sayıların toplamı:" + toplam.ToString());
+            }
+            catch(Exception hata)
+            {
+                MessageBox.Show("Lütfen değerlerinizi kontrol ediniz"+hata.ToString());
+            }
+            finally //hata yoksa try+finally bloğu çalışır. hata varsa catch+finally bloğu çalışır
+            {
+                MessageBox.Show("Finally kodu çalıştı");
+            }
+
+
+        }
+    }
+}
